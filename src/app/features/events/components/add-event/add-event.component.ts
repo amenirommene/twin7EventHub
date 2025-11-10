35d3b10b9c9d7c581ea442fef601a99cb21de606
amenir88 <ameni.rommene@esprit.tn>
2025-11-10 11:42:20 +0100

    Suite reactive form - validateur personnalisé

--- src/app/features/events/components/add-event/add-event.component.ts
+++ src/app/features/events/components/add-event/add-event.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { futurDateValidator } from '../../../../shared/validators/futur-date.validator';
 
 @Component({
   selector: 'app-add-event',
@@ -15,18 +16,25 @@ export class AddEventComponent {
 
    this.myForm=new FormGroup({
     description : new FormControl('', Validators.required),
-    title:new FormControl('',[Validators.required,Validators.minLength(10)])
+    title:new FormControl('',[Validators.required,Validators.minLength(10)]),
+    date:new FormControl('',[Validators.required,futurDateValidator(7)]),
+    price : new FormControl('', [Validators.required, Validators.pattern("^\\d+(\\.\\d+)?$")
+    ])
 
    });
 
 
    this.myForm2=new FormGroup({
     title:new FormControl(),
-    description : new FormControl()
+    description : new FormControl(),
+
    });
   }
 
   get title(){
     return this.myForm.get('title');  }
-
+     get price(){
+    return this.myForm.get('price');  }
+  get date(){
+    return this.myForm.get('date');  }
 }
